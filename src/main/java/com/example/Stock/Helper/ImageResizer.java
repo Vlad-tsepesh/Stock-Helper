bf9eba4b6d828c4837edca9bb318955fb7a318b1
vladtsepesh <vladtsepesh.wow@gmail.com>
2025-08-24 15:38:55 +0300

    implemented image resizer

---
+++ src/main/java/com/example/Stock/Helper/ImageResizer.java
@@ -0,0 +1,35 @@
+package com.example.Stock.Helper;
+
+import org.apache.commons.io.output.ByteArrayOutputStream;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class ImageResizer {
+
+    public static Resource resize(String inputPath, int maxSize) throws IOException {
+        ClassPathResource resource = new ClassPathResource("bike.jpg");
+        BufferedImage originalImage = ImageIO.read(resource.getInputStream());
+
+        // calculate proportional scale
+        double scale = (double) maxSize / originalImage.getWidth();
+
+        int targetWidth = (int) (originalImage.getWidth() * scale);
+        int targetHeight = (int) (originalImage.getHeight() * scale);
+
+        BufferedImage resizedImage = new BufferedImage(targetWidth, targetHeight, BufferedImage.TYPE_INT_RGB);
+        Graphics2D graphics2D = resizedImage.createGraphics();
+        graphics2D.drawImage(originalImage, 0, 0, targetWidth, targetHeight, null);
+        graphics2D.dispose();
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(resizedImage, "png", baos);
+        return new ByteArrayResource(baos.toByteArray());
+    }
+}
---
+++ src/main/java/com/example/Stock/Helper/commands/ImageToStockDataCommand.java
@@ -0,0 +1,31 @@
+package com.example.Stock.Helper.commands;
+
+import com.example.Stock.Helper.ImageResizer;
+import com.example.Stock.Helper.service.StockDescriptionService;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+@ShellComponent
+public class ImageToStockDataCommand {
+
+    private final StockDescriptionService descriptionService;
+
+    public ImageToStockDataCommand(StockDescriptionService descriptionService) {
+        this.descriptionService = descriptionService;
+    }
+
+    @ShellMethod(key = "generate-metadata", value = "Generate stock metadata from an image path")
+    public ChatResponse hello(@ShellOption(defaultValue = "/bike.jpg") String arg) throws IOException {
+        Resource resizedResource = ImageResizer.resize(arg, 300);
+        return descriptionService.generateDescription(resizedResource);
+    }
+}
